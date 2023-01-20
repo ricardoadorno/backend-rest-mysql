cf80a3ee621e384cacd387f45ae9f4fbcf35a098
Ricardo Adorno <ricardo.castrorc1998@gmail.com>
2023-01-20 13:18:04 -0300

    INIT

---
+++ src/main/java/com/api/restapipostgress/RestApiPostgressApplication.java
@@ -0,0 +1,13 @@
+package com.api.restapipostgress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiPostgressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiPostgressApplication.class, args);
+	}
+
+}
