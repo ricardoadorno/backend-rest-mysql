20fcfff95d1e71e14ef4c3d317ea07b5022b5c5e
Ricardo Adorno <ricardo.castrorc1998@gmail.com>
2023-01-23 12:12:41 -0300

    FIX: fixed crud

--- src/main/java/com/api/restapipostgress/User.java
+++ src/main/java/com/api/restapipostgress/User.java
@@ -4,13 +4,44 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    private String firstName;
+    private String lastName;
+
+    public User() {
+
+    }
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Column(name = "first_name", nullable = false)
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Column(name = "last_name", nullable = false)
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
 }
--- src/main/java/com/api/restapipostgress/UserController.java
+++ src/main/java/com/api/restapipostgress/UserController.java
@@ -1,30 +1,27 @@
 package com.api.restapipostgress;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping("/api")
 public class UserController {
     @Autowired
-    private UserService userService;
+    private UserRepository userRepository;
 
     @GetMapping("/users")
-    public ResponseEntity<Iterable<User>> getAllUsers() {
-        Iterable<User> users = userService.getAllUsers();
-
-        return new ResponseEntity<>(users, HttpStatus.OK);
+    public Iterable<User> getAllUsers() {
+        return userRepository.findAll();
     }
 
     @PostMapping("/users")
-    public ResponseEntity<User> createUser(@RequestBody User user) {
-        User createdUser = userService.createUser(user);
-
-        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
-
+    public User createUser(@Validated @RequestBody User user) {
+        return userRepository.save(user);
     }
+
 }
